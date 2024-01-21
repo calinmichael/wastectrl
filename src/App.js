33926ded4eb25542fdf55ab349fae9e3273cb650
calinmichael <ccascaval7@gmail.com>
2024-01-20 22:42:03 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Mission from './components/pages/Mission';
+import Contact from './components/pages/Contact';
+import Shop from './components/pages/Shop';
+import React, { useRef, useEffect } from "react";
+import About from './components/pages/About';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Navbar/>
+    <Routes>
+    <Route path='/' exact element={ <Home />}></Route>  
+    <Route path='/mission' exact element={ <Mission />}></Route>
+    <Route path='/contact' exact element={ <Contact />}></Route>
+    <Route path='/shop' exact element={ <Shop />}></Route>
+    <Route path='/about' exact element={ <About />}></Route>
+
+    </Routes>
+    </Router>
+     
+
+    </>
   );
 }
 
 export default App;
+
